12d3f04ccd2c8c293004f73c2ee21d92747527dd
mohs-in <in.mohsin@outlook.com>
2024-03-09 16:54:27 +0530

    components and images added

---
+++ components/Hero.jsx
@@ -0,0 +1,27 @@
+import { getImageUrl } from "../src/imgs"
+
+
+function Hero() {
+    return (
+        <main>
+            <div className="main">
+                <div className="main--input">
+                    <div className="main--input-div">
+                        <label className="main--input-text">Top Text</label>
+                        <input className="main--input-field" type="text" /></div>
+                    <div className="main--input-div">
+                        <label className="main--input-text">Bottom Text</label>
+                        <input className="main--input-field" type='text' /></div>
+                </div>
+                <div>
+                    <button className="main--button">Get a new meme image</button>
+                </div>
+                <div>
+                    <img className='main--img' src={getImageUrl('g1')} alt="Meme"  />
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Hero
---
+++ components/Nav.jsx
@@ -0,0 +1,15 @@
+import {getImageUrl} from "../src/imgs.js";
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src={getImageUrl('mlogo')}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
